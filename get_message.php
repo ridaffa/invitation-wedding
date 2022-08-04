330aba3586799526638f3bee90077e6380feb0e1
ridaffa <47104358+ridaffa@users.noreply.github.com>
2022-08-04 12:33:56 +0700

    aw

--- get_message.php
+++ get_message.php
@@ -1,7 +1,22 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$url = './message/message.json';
-$readJSONFile = file_get_contents($url);
-$array = json_decode($readJSONFile);
-echo json_encode($array);
+// $url = './message/message.json';
+// $readJSONFile = file_get_contents($url);
+// $array = json_decode($readJSONFile);
+$url = 'https://getpantry.cloud/apiv1/pantry/8c6b1475-4c16-4496-8249-64627e20e5d3/basket/weddingmessage';
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'GET'
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { 
+    echo json_encode(null);  
+}else{
+    echo json_encode(json_decode($result)); 
+}
 ?>
--- new_message.php
+++ new_message.php
@@ -1,20 +1,34 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$url = './message/message.json';
-$readJSONFile = file_get_contents($url);
-$array = json_decode($readJSONFile);
+$array = array('message' => array());
 $message= new stdClass();
 $message->name = $_POST['name'];
 $message->message = htmlspecialchars($_POST['message']);
 $message->time = $_POST['time'];
-
 $message->status = 'done';
 if(strlen($_POST['message']) >= 500 || strlen($_POST['message']) <= 4 || strlen($_POST['name']) >= 50 || strlen($_POST['name']) <= 4){
     $message->status = 'error';
 }else{
-    array_push($array->message,$message);
-$data = json_encode($array);
-file_put_contents($url, $data);
+    array_push($array['message'],$message);
+    $data = json_encode($array);
+    $url = 'https://getpantry.cloud/apiv1/pantry/8c6b1475-4c16-4496-8249-64627e20e5d3/basket/weddingmessage';
+    //The data you want to send via POST
+
+    //open connection
+    $ch = curl_init();
+
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch,CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch,CURLOPT_POST, true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+    //So that curl_exec returns the contents of the cURL; rather than echoing it
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+    //execute post
+    $result = curl_exec($ch);
+    echo json_encode($message);
 }
-echo json_encode($message);
 ?>
